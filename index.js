3f1cbe14e249844fc7a2484e23aef8371c57838f
GotoXY-dev <20201012000256@pucgo.edu.br>
2021-02-15 02:12:49 -0300

    Ricardo sacou um valor

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ class Cliente{
 class ContaCorrente{
     agencia;
     saldo;
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+        
+    }
 }
 
 const cliente1 = new Cliente();
@@ -22,12 +28,20 @@ cliente2.cpf = 12341234;
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
+
+
 console.log(contaCorrenteRicardo.saldo);
 contaCorrenteRicardo.saldo = 100;
 console.log(contaCorrenteRicardo.saldo);
-let valorSacado = 200;
+contaCorrenteRicardo.sacar(50);
 
-contaCorrenteRicardo.saldo -= valorSacado;
+let valorSacado = 200;
+if(contaCorrenteRicardo.saldo >= valorSacado){
+    contaCorrenteRicardo.saldo -= valorSacado;
+}
+
+
+
 console.log(contaCorrenteRicardo.saldo);
 console.log(cliente1);
 console.log(cliente2);
