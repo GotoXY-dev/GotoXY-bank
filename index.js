a4a17e7b52d629c81a73f55410ed437133622528
GotoXY-dev <20201012000256@pucgo.edu.br>
2021-02-14 21:44:07 -0300

    Evitando repeticoes

---
+++ index.js
@@ -0,0 +1,12 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Joao";
+const cliente2CPF = 11122233310;
+const cliente2Agencia = 1002;
+const cliente2Saldo = 0;
+
+
+console.log(cliente1Nome);
